2220b102efb765f74ca7da86af86fa2e995da7fe
Ben <benbromley.104@gmail.com>
2021-06-25 21:05:40 +0000

    rm default content and replaced with my own

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './styles/styles.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="overlay"></div>
+            <nav id="nav-container">
+                <ul id="navigation">
+                    <li><a>About</a></li>
+                    <li><a>Portfolio</a></li>
+                    <li><a>Contact Me</a></li>
+                </ul>
+            </nav>
+            <div id="title-container">
+                <h1 id="title">I design and build websites to help you grow your business</h1>
+            </div>
+            <div id="intro-text-container">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At beatae vero quas, fugiat voluptas mollitia itaque dolor quibusdam ipsum sapiente et nesciunt minus ut totam consequatur, quisquam amet commodi suscipit laborum natus magni libero qui soluta! Iste eos cum assumenda?</p>
+            </div>
       </header>
     </div>
   );
