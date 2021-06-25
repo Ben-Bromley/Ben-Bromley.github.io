440c4b8a144062c60ed53335fbdf61d0009b855b
Ben (Macbook) <benbromley.104@gmail.com>
2021-06-25 23:26:22 +0100

    converted to jsx and added data

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './styles/index.css';
+
+function App() {
+  return (
+    <div>
+      <header class="main-header">
+        <div id="overlay"></div>
+        <div id="nav-container">
+          <nav>
+            <ul id="navigation">
+              <li><a href="#">About</a></li>
+              <li><a href="#">Portfolio</a></li>
+              <li><a href="#">Get In Touch</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div id="title-container">
+          <h1 id="title">I design and build websites to help you grow your busniess</h1>
+        </div>
+        <div id="intro-text-container">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed tenetur delectus earum dignissimos, eum eveniet fuga quos dolor veritatis, quisquam molestiae, nihil facilis. Rem quasi totam nostrum. Nam quos repellat, fugiat omnis eveniet culpa voluptas sequi rem tenetur reprehenderit. A.</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
