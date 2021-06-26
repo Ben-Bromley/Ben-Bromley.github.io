75633d84132b07c2bcc6a127d39cb430fb328efd
Ben (Macbook) <benbromley.104@gmail.com>
2021-06-26 14:13:27 +0100

    added next content

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import './styles/index.css';
+import Nav from './components/nav.jsx';
 
 function App() {
   return (
     <div>
       <header class="main-header">
         <div id="overlay"></div>
-        <div id="nav-container">
-          <nav>
-            <ul id="navigation">
-              <li><a href="#">About</a></li>
-              <li><a href="#">Portfolio</a></li>
-              <li><a href="#">Get In Touch</a></li>
-            </ul>
-          </nav>
-        </div>
+        <Nav />
         <div id="title-container">
           <h1 id="title">I design and build websites to help you grow your busniess</h1>
         </div>
@@ -21,6 +14,10 @@ function App() {
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed tenetur delectus earum dignissimos, eum eveniet fuga quos dolor veritatis, quisquam molestiae, nihil facilis. Rem quasi totam nostrum. Nam quos repellat, fugiat omnis eveniet culpa voluptas sequi rem tenetur reprehenderit. A.</p>
         </div>
       </header>
+      <div>
+        <h2>About Me</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat hic nihil unde delectus maiores nisi! In quae neque ut porro beatae atque voluptate tempora nulla provident quibusdam aut accusamus minima nostrum architecto possimus quaerat inventore omnis molestias, enim doloremque qui.</p>
+      </div>
     </div>
   );
 }
