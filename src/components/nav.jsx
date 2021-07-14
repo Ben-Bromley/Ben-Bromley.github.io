42a5c91df735b0ac8c30d45197a839b47d8debee
Ben (Macbook) <benbromley.104@gmail.com>
2021-07-14 18:45:12 +0100

    formatted component

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,17 +1,25 @@
 import '../styles/index.css';
 
 function Nav() {
-    return (
-        <div id="nav-container" className="container">
-            <nav>
-                <ul id="navigation">
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Portfolio</a></li>
-                    <li><a href="#" class="highlight-link">Get In Touch</a></li>
-                </ul>
-            </nav>
-        </div>
-    );
+	return (
+		<div id="nav-container" className="container">
+			<nav>
+				<ul id="navigation">
+					<li>
+						<a href="#about">About</a>
+					</li>
+					<li>
+						<a href="#portfolio">Portfolio</a>
+					</li>
+					<li>
+						<a href="#get-in-touch" className="highlight-link">
+							Get In Touch
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
 }
 
 export default Nav;
